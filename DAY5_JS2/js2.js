fe056cd3f5f4c4ffd86b122fde76064f7cfc29d6
unknown <wchen28@wgu.edu>
2024-11-30 23:22:43 -0500

    js2

---
+++ DAY5_JS2/js2.js
@@ -0,0 +1,54 @@
+// For loop
+// const numbers = [1, 2, 3, 4, 5];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// forEach
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(function(number) {
+//     console.log(number);
+// });
+
+// for…of…
+// const numbers = [1, 2, 3, 4, 5];
+// for (const number of numbers) {
+//     console.log(number);
+// }
+
+// Array.prototype.map
+// const numbers = [1, 2, 3, 4, 5];
+
+// // Using map to double each number in the array
+// const doubledNumbers = numbers.map(function(number) {
+//     return number * 2;
+// });
+
+// console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+
+
+
+// const person = { firstName: 'Alice', lastName: 'Smith' };
+
+// // Object destructuring with renaming
+// const { firstName: first, lastName: last } = person;
+
+// console.log(first); // Output: 'Alice'
+// console.log(last); // Output: 'Smith'
+
+
+// const numbers = [1, 2, 3];
+// const [a, b, c] = numbers;
+
+
+// const [a, b, ...rest] = [1, 2, 3, 4, 5];
+// console.log(rest); // Output: [3, 4, 5]
+
+// Shallow spread operator
+// const originalArray = [1, 2, 3];
+// const shallowCopy = [...originalArray];
+
+// Deep copy, clonedeep function
+// const _ = require('lodash');
+// const originalObject = { a: 1, b: { c: 2 } };
+// const deepCopy = _.cloneDeep(originalObject);
