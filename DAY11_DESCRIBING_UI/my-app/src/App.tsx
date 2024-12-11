fd7444c0284963914d39c28388dd0ff6afc9481a
unknown <wchen28@wgu.edu>
2024-12-10 22:35:01 -0500

    day11_done

---
+++ DAY11_DESCRIBING_UI/my-app/src/App.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+  const setZero = () => {
+    setCount(0);
+  };
+
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const handleMouseOver = () => {
+    setIsDisabled(true);
+  };
+  const handleMouseLeave = () => {
+    setIsDisabled(false);
+  };
+
+  // <boolean> falseckable button
+  // Create a button, when hovered, it should become disabled
+  // Show/Hide Paragraph
+  // Create a component with a button and a paragraph of text. Clicking the button should toggle the visibility of the paragraph.
+  const [showText, setShowText] = useState<boolean>(true);
+
+  const handleShowText = () => {
+    setShowText(!showText);
+  };
+  return (
+    <>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={decreaseCount}>decrease</button>
+        <button onClick={increaseCount}>increase</button>
+        <button onClick={setZero}>zero</button>
+        <button
+          onMouseOver={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+          disabled={isDisabled}
+        >
+          unclickable button
+        </button>
+      </div>
+      <div>
+        {showText && <p>Click on the follow button and see what happens</p>}
+        <button onClick={handleShowText}>show/hide</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
